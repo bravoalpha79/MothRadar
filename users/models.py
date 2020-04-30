edd11b90763ce28cb9311f9d9551870a7a2dfcbb
Igor Basuga <igor.basuga@gmail.com>
2020-04-30 20:11:03 +0200

    add Profile model to users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import datetime
 
-# Create your models here.
+# Profile model created using Corey Schafer's YT tutorial
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
