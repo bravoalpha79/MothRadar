09cbd1713dec7e8cec0cdb32f68475259359c315
Igor Basuga <igor.basuga@gmail.com>
2020-05-02 10:18:30 +0200

    add email field to register form; add crispy style

--- users/views.py
+++ users/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegistrationForm
 
 
 # Create your views here.
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get("username")
             messages.success(
                 request, "Your account has been created, {}.".format(username)
             )
+            form.save()
             return redirect("home")
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
     return render(request, "users/register.html", {"form": form})
