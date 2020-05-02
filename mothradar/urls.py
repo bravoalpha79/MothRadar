35fc4e1862939001ca0ec81d361f186ea8c6ec0d
Igor Basuga <igor.basuga@gmail.com>
2020-05-02 12:54:18 +0200

    add login and logout urls and templates; add profile view

--- mothradar/urls.py
+++ mothradar/urls.py
@@ -15,10 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 from users import views as users_views
 
 urlpatterns = [
     path("", include("tickets.urls")),
     path("register/", users_views.register, name="register"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="users/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="users/logout.html"),
+        name="logout",
+    ),
+    path("profile/", users_views.profile, name="profile"),
     path("admin/", admin.site.urls),
 ]
