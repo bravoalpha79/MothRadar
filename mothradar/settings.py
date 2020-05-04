21198520da15786695e142058af2d42140d21f1f
Igor Basuga <igor.basuga@gmail.com>
2020-05-04 11:30:28 +0200

    add Comments app

--- mothradar/settings.py
+++ mothradar/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "users.apps.UsersConfig",
     "tickets.apps.TicketsConfig",
+    "comments.apps.CommentsConfig",
     "crispy_forms",
     "django.contrib.admin",
     "django.contrib.auth",
