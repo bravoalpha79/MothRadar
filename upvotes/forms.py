412b4dd5306d3a616571614db9595d5d97d92d30
Igor Basuga <igor.basuga@gmail.com>
2020-05-09 12:00:07 +0200

    add Upvote Payment Form

---
+++ upvotes/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+class UpvoteFeaturePaymentForm(forms.Form):
+
+    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+    YEAR_CHOICES = [(i, i) for i in range(2020, 2030)]
+
+    credit_card_number = forms.CharField(label="Credit Card number", required=False)
+    cvv = forms.CharField(label="Security Code (CVV)", required=False)
+    expiry_month = forms.ChoiceField(
+        label="Expiry month", choices=MONTH_CHOICES, required=False
+    )
+    expiry_year = forms.ChoiceField(
+        label="Expiry year", choices=YEAR_CHOICES, required=False
+    )
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
